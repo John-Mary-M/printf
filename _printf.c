a293b64efe69317bfbb00b8eaaccfeae28448261
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-04-22 11:16:05 -0700

    _printf function

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ *_printf - formatted output conversion and print data.
+ * @format: input string.
+ *Return: number of chars printed.
+ */
+int _printf(const char *format, ...)
+{
+int i, rcount = 0;
+va_list args;
+
+if (format == NULL)
+return (-10);
+va_start(args, format);
+for (i = 0; format[i]; i++)
+{
+if (format[i] != '%')
+{
+_putchar(format[i]);
+rcount++;
+}
+else
+{
+switch (format[++i])
+{
+case 'c':
+_putchar(va_arg(args, int));
+rcount++;
+break;
+case 's':
+/*
+*_putchar(va_arg(args, int));
+*/
+rcount += print_str(va_arg(args, char *));
+break;
+case '%':
+_putchar('%');
+rcount++;
+break;
+}
+}
+}
+va_end(args);
+return (rcount);
+}
